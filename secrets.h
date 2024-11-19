e195f61e0e4db78c9d6d77c15e8a18b28a615ba2
LeoSvahn <145968827+LeoSvahn@users.noreply.github.com>
2024-11-19 14:12:06 +0100

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -1,4 +1,4 @@
-#define SECRET_SSID "R32-7723" // Ditt nätverksnamn
-#define SECRET_PASS "tuwxn78976" // Ditt nätverkslösenord
+#define SECRET_SSID "xx" // Ditt nätverksnamn
+#define SECRET_PASS "xx" // Ditt nätverkslösenord
 #define SECRET_CH_ID 2709081 // Din ThingSpeak Channel ID
 #define SECRET_WRITE_APIKEY "U72WVCGH5C950AKI" // Din ThingSpeak Write API Key
